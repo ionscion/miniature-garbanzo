64b26809c730724d4eacbed2b37b0931b133ad5f
jancypp <120283327+jancypp@users.noreply.github.com>
2023-02-02 21:10:58 -0700

    created shell HTML, updated psuedocode, connected bulma css and js files

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
 //I want to find music events close to my location
+    //create a search bar with a button
 //I want to be able to search based on my location
-//I want to be able to search based on artist
-//I want to be able to search based on venue
-//I want to be able hear the artist 
-    //-link to music API e.g. spotify, youtube, apple music
-//APIs 1) pollstar, AXS, ticketmaster 2) googlemaps, apple maps 3)spotify, youtube, apple music
-//I want to see available concerts based on my entry (location, artist, venue)
-//I want to be provided with link
+    //enter and store the city and state
+    //get longitude and latitude of city and state -- open weather geo location api
+    //pass long and lat through seatgeek and get list of artists and venues
+// Display that info/text on the cards
+    //select a button for a artist they're interested in seeing
+    //store that locally
+//I want my saved options rendered on the page on reload in favorites section
+//I want to be able to clear favorites on demand of my saved options
+// I want the screen size to be responsive - mobile first/css/html
+
+
+
+
+
