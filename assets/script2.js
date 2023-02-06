82da084def861a7dae397b2b8328a3bbc9977c6e
bensboto <bensboto@gmail.com>
2023-02-05 20:31:03 -0700

    updated html order for buttons in search bar, added function to clear the search before running and also clear the entered data after each submission

--- assets/script2.js
+++ assets/script2.js
@@ -3,18 +3,20 @@ let longitude = [];
 let searchButton = document.getElementById("search-btn");
 let ipSearchButton = document.getElementById("ip-search-btn");
 let columnData = document.getElementById("column-1");
+let clearButton = document.getElementById("clear-btn");
+let cityInputElem = document.getElementById("city-input");
+let selectedStateElem = document.getElementById("state-input");
 
 let weatherapi = "326e6d35f7ebe093972477e3b80624aa";
 let seatgeekapi = "MzE3NDAyMDR8MTY3NTM1NDU3My4xNzIzODQ";
 let ipGeo = "38673022df5a4cfdbd8d9da66bf8a214";
 
+//make city and state mandatory
+
 function getCityApi(evt) {
   evt.preventDefault();
-  let cityInput = document.getElementById("city-input");
-  let selectedState = document.getElementById("state-input");
-
-  cityInput = cityInput.value;
-  selectedState = selectedState.value;
+  let cityInput = cityInputElem.value;
+  let selectedState = selectedStateElem.value;
 
   if (cityInput === "New York" || cityInput === "new york") {
     cityInput = "City of New York";
@@ -39,20 +41,18 @@ function getCityApi(evt) {
         }
       }
       // this will be the function Nate is working on below
-
       getConcertApi(latitude, longitude);
     })
     .catch(function (error) {
       console.error("There was a problem with the fetch operation:", error);
     });
   // searchSave(cityInput, selectedState);
-  cityInput.value = "";
-  selectedState.value = "";
+  clearSearch();
 }
 
 //temporary to be removed when Nate add his functions
 function getConcertApi() {
-    clearPage();
+  clearPage();
   let seatGeekUrl = `https://api.seatgeek.com/2/events?lat=${latitude}&lon=${longitude}&range=5mi&taxonomies.name=concert&client_id=${seatgeekapi}`;
   fetch(seatGeekUrl)
     .then(function (response) {
@@ -88,16 +88,18 @@ function getCurrentIpApi() {
 }
 function clearPage() {
   columnData.textContent = "";
-//   for (let i = 0; i < 5; i++) {
-//     let currentId = document.getElementById(`day-${i + 1}`);
-//     currentId.textContent = "";
-  
-//   }
+  //   for (let i = 0; i < 5; i++) {
+  //     let currentId = document.getElementById(`day-${i + 1}`);
+  //     currentId.textContent = "";
+  //   }
 }
 
+function clearSearch() {
+  cityInputElem.value = "";
+  selectedStateElem.value = "";
+  console.log("clear search ran");
+}
+
+clearButton.addEventListener("click", clearSearch);
 searchButton.addEventListener("click", getCityApi);
 ipSearchButton.addEventListener("click", getCurrentIpApi);
-/*
-Todo:
-
-*/
